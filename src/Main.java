1edc03af1550c36b22126f6b3b6b7471756f9843
Javier <javierraposohuelva@gmail.com>
2025-11-06 17:09:33 +0100

    add main task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-import java.io.*;        // ObjectOutputStream / ObjectInputStream
+import java.io.*;
 import java.nio.file.*;  // Paths y Files
-import java.util.*;      // List y ArrayList
+import java.util.ArrayList;
+
 
 public class Main {
 
@@ -10,14 +11,36 @@ public class Main {
     public static void main(String[] args) {
         // Realiza las siguientes tareas, agrupa en funciones cuando veas oportuno
         //TODO: 1) Crear una lista de personas
-
         //TODO: 2) Añadir personas (por consola)
-
         //TODO: 3) Serializar la lista al fichero
-
         //TODO: 4) Deserializar la lista desde el fichero
-
         //TODO: 5) Imprimir por consola las personas deserializadas
 
+        Persona persona = new Persona("Manolo", "ROCOSO", 29);
+        ArrayList arrayList = new ArrayList<Persona>();
+        
+        try {
+            FileOutputStream fos = new FileOutputStream("personas.dat");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(persona);
+            oos.close();
+            System.out.println("Objeto Serializado Correctamente");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+        try {
+            FileInputStream fis = new FileInputStream("personas.dat");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+
+            Persona personaLeida = (Persona) ois.readObject();
+
+            System.out.println("Objeto deserializado correctamente: " + personaLeida);
+            ois.close();
+        } catch (IOException | ClassNotFoundException e ) {
+            e.printStackTrace();
+        }
     }
 }
--- src/Persona.java
+++ src/Persona.java
@@ -1,9 +1,82 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
 
-/**
-* Esta clase ...
-* 
-*@Autor: Javier Raposo Huelva
-* @Version: 2025:11
-*/public class Persona {
+public class Persona implements Serializable {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String email;
+    private String telefono;
+
+    /*
+     * TODO: Crea el constructor de la clase Persona que reciba como parámetro una cadena
+     * de caracteres con los valores separados por comas (,)
+     * Ejemplo de cadena: "Ana,Perez,25,ana.perez@example.com,+34911111111"
+     *
+     * El constructor debe:
+     * 1. Separar los valores usando split(",").
+     * 2. Asignar los valores a los atributos correspondientes.
+     * 3. Convertir el tercer valor (edad) a entero.
+     * 4. No lanzar excepciones. Si algo falla, asigna valores por defecto.
+     */
+
+    public Persona(String csvData) {
+        String[] values = csvData.split(",");
+        try {
+            this.nombre = values[0];
+        } catch (Exception e) {
+            this.nombre = "";
+        }
+        try {
+            this.apellido = values[1];
+        } catch (Exception e) {
+            this.apellido = "";
+        }
+        try {
+            this.edad = Integer.parseInt(values[2]);
+        } catch (Exception e) {
+            this.edad = 0;
+        }
+    }
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
 }
